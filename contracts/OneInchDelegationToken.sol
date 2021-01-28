a5852d44c520114dcdb8686b2508db99478f787f
ninjadevtrack <alrubinstien1088@gmail.com>
2021-01-28 09:57:24 +0900

    Inherit GovernancePowerDelegationERC20 into the OneInchDelegationToken

--- contracts/OneInchDelegationToken.sol
+++ contracts/OneInchDelegationToken.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.0;
 
+import { GovernancePowerDelegationERC20 } from './GovernancePowerDelegationERC20.sol';
 
 /**
  * The OneInchDelegationToken contract does this and that...
  */
-contract OneInchDelegationToken {
+contract OneInchDelegationToken is GovernancePowerDelegationERC20 {
 
     constructor() public {
 
