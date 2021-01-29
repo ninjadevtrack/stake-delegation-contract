d09b0aca0642334058a669a4b2902bf11f9190e0
ninjadevtrack <alrubinstien1088@gmail.com>
2021-01-29 22:08:56 +0900

    Replace PROPOSAL_POWER with STAKE and DISTRIBUTION in the DelegationType enum

--- contracts/stake-delegation/commons/StakeDelegationObjects.sol
+++ contracts/stake-delegation/commons/StakeDelegationObjects.sol
@@ -5,6 +5,6 @@ contract StakeDelegationObjects {
     /**
      * @notice - delegationType the type of delegation (VOTING_POWER, PROPOSITION_POWER)
      */
-    enum DelegationType { VOTING_POWER, PROPOSITION_POWER }
+    enum DelegationType { STAKE, VOTING_POWER, DISTRIBUTION }
 
 }
