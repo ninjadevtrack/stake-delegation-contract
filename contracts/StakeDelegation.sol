1ad1e6ddadfdb9f8f2b153343b80c4cfa643f73c
ninjadevtrack <alrubinstien1088@gmail.com>
2021-01-29 21:39:35 +0900

    Define StakeDelegation.sol

---
+++ contracts/StakeDelegation.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.6.0;
+
+
+/**
+ * @notice - A liquidity protocol stake delegation contract
+ */
+contract StakeDelegation {
+
+    constructor() public {}
+    
+}
