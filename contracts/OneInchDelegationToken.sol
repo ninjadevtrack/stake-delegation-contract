13324361211f280f94eafc1504b2471fd745525b
ninjadevtrack <alrubinstien1088@gmail.com>
2021-01-29 09:39:55 +0900

    Remove inheritance of GovernancePowerDelegationERC20.sol from OneInchDelegationToken.sol

--- contracts/OneInchDelegationToken.sol
+++ contracts/OneInchDelegationToken.sol
@@ -1,15 +1,22 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.0;
 
-import { GovernancePowerDelegationERC20 } from './GovernancePowerDelegationERC20.sol';
+import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+//import { GovernancePowerDelegationERC20 } from './GovernancePowerDelegationERC20.sol';  <--Just reference
+
 
 /**
- * The OneInchDelegationToken contract does this and that...
+ * @notice - The OneInchDelegationToken contract does this and that...
+ * @dev -  
  */
-contract OneInchDelegationToken is GovernancePowerDelegationERC20 {
+contract OneInchDelegationToken is ERC20 {
 
-    constructor() public {
+    string NAME = 'OneInch Delegation Token';
+    string SYMBOL = '1INCH DELEGATION';
+    uint8 DECIMALS = 18;
 
+    constructor() public ERC20(NAME, SYMBOL) {
+        /// [Todo]: Add somthing to here
     }
-
+    
 }
