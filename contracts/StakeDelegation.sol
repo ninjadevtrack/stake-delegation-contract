41146b61bb31ec658aec133258abef1c272acbf0
ninjadevtrack <alrubinstien1088@gmail.com>
2021-01-29 21:53:57 +0900

    Define delegate() in the StakeDelegation.sol

--- contracts/StakeDelegation.sol
+++ contracts/StakeDelegation.sol
@@ -1,12 +1,29 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.6.0;
 
+import { OneInch } from "./1inch/1inch-token/OneInch.sol";
+
 
 /**
  * @notice - A liquidity protocol stake delegation contract
  */
 contract StakeDelegation {
 
-    constructor() public {}
+    mapping (address => address[]) delegationAddresses;
+
+    OneInch public oneInch; /// 1inch Token
+
+    constructor(OneInch _oneInch) public {
+        oneInch = _oneInch;
+    }
+
+    /**
+     * @notice - Register a delegation address
+     */ 
+    function delegate(address delegationAddress) public returns (bool) {
+        /// Add a new token holder address which ask delegation
+        delegationAddresses[delegationAddress].push(msg.sender);
+    }
     
+
 }
