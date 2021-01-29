a1860c312b5fd91114f3b780dd8f46ec84daf68d
ninjadevtrack <alrubinstien1088@gmail.com>
2021-01-29 22:05:05 +0900

    Define the DelegationType enum

--- contracts/stake-delegation/commons/StakeDelegationObjects.sol
+++ contracts/stake-delegation/commons/StakeDelegationObjects.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.6.0;
 
 contract StakeDelegationObjects {
-    
+
+    /**
+     * @notice - delegationType the type of delegation (VOTING_POWER, PROPOSITION_POWER)
+     */
+    enum DelegationType { VOTING_POWER, PROPOSITION_POWER }
+
 }
