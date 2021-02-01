646eeccd4131285d696b483a06660b82a6fbeec2
ninjadevtrack <alrubinstien1088@gmail.com>
2021-02-01 11:56:22 +0900

    Remove extra code from StakeDelegationStorages/Events/Objects/Constants.sol

--- contracts/stake-delegation/commons/StakeDelegationStorages.sol
+++ contracts/stake-delegation/commons/StakeDelegationStorages.sol
@@ -4,14 +4,4 @@ import { StakeDelegationObjects } from "./StakeDelegationObjects.sol";
 
 contract StakeDelegationStorages is StakeDelegationObjects {
 
-    mapping(address => mapping(uint256 => Checkpoint)) checkpoints;  /// checkpoints
-    mapping(address => uint256) checkpointsCounts;                   /// checkpoints count
-    mapping(address => address) delegates;                           /// delegatees list
-
-    /// @notice A record of votes checkpoints for each account, by index
-    //mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;  /// [Key]: userAddress -> 
-
-    /// @notice The number of checkpoints for each account
-    //mapping (address => uint32) public checkpointsCounts;
-
 }
